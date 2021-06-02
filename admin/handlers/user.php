16a82b22fe0857a8eeac2591d905aaf62469331a
harisfi <acc.cp.haris@gmail.com>
2021-06-02 19:58:54 +0700

    Add backend for user

---
+++ admin/handlers/user.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+include("../../koneksi/koneksi.php");
+print_r($_POST);
+if (isset($_POST['user']) && $_POST['user'] == "edit") {
+    $id = mysqli_real_escape_string($koneksi, $_POST['id']);
+    $id_a = mysqli_real_escape_string($koneksi, $_POST['id_a']);
+    $id_p = mysqli_real_escape_string($koneksi, $_POST['id_p']);
+    $nama = mysqli_real_escape_string($koneksi, $_POST['nama']);
+    $tgl_lahir = mysqli_real_escape_string($koneksi, $_POST['tgl_lahir']);
+    $no_hp = mysqli_real_escape_string($koneksi, $_POST['no_hp']);
+    $email = mysqli_real_escape_string($koneksi, $_POST['email']);
+    $status = mysqli_real_escape_string($koneksi, $_POST['status']);
+    $alamat1 = mysqli_real_escape_string($koneksi, $_POST['alamat1']);
+    $alamat2 = mysqli_real_escape_string($koneksi, $_POST['alamat2']);
+    $prov = mysqli_real_escape_string($koneksi, $_POST['prov']);
+    $kota = mysqli_real_escape_string($koneksi, $_POST['kota']);
+    $kec = mysqli_real_escape_string($koneksi, $_POST['kec']);
+    $kel = mysqli_real_escape_string($koneksi, $_POST['kel']);
+    $kd_pos = mysqli_real_escape_string($koneksi, $_POST['kd_pos']);
+    $provider = mysqli_real_escape_string($koneksi, $_POST['provider']);
+    $no_rek = mysqli_real_escape_string($koneksi, $_POST['no_rek']);
+    if (!empty($id) && !empty($id_a) && !empty($id_p) && !empty($nama) && !empty($tgl_lahir) && !empty($no_hp) && !empty($email) && !empty($status) && !empty($prov) && !empty($kota) && !empty($kec) && !empty($kel) && !empty($kd_pos) && !empty($provider) && !empty($no_rek)) {
+        $status = $status == "on" ? "1" : "0";
+
+        $query_u = "UPDATE tbl_user SET nama = '$nama', tanggal_lahir = '$tgl_lahir', no_hp = '$no_hp', email = '$email', verified = '$status', updated_at = current_timestamp(), deleted = null, deleted_at = null WHERE id = '$id'";
+        $ret_u = mysqli_query($koneksi, $query_u);
+        $jum_u = mysqli_affected_rows($koneksi);
+        if ($jum_u > 0) {
+            $query_a = "UPDATE tbl_alamat_user SET alamat1 = '$alamat1', alamat2 = '$alamat2', provinsi = '$prov', kota = '$kota', kecamatan = '$kec', kelurahan = '$kel', kode_pos = '$kd_pos', updated_at = current_timestamp(), deleted = null, deleted_at = null WHERE id = '$id_a'";
+            $ret_a = mysqli_query($koneksi, $query_a);
+            $jum_a = mysqli_affected_rows($koneksi);
+            if ($jum_a > 0) {
+                $query_p = "UPDATE tbl_pembayaran SET id_provider = '$provider', no_rek = '$no_rek', updated_at = current_timestamp(), deleted = null, deleted_at = null WHERE id = '$id_p'";
+                $ret_p = mysqli_query($koneksi, $query_p);
+                $jum_p = mysqli_affected_rows($koneksi);
+                if ($jum_p > 0) {
+                    header("Location:../user.php?m=s-1");
+                } else {
+                    header("Location:../user-edit.php?id=$id&m=d-4");
+                }
+            } else {
+                header("Location:../user-edit.php?id=$id&m=d-3");
+            }
+        } else {
+            header("Location:../user-edit.php?id=$id&m=d-2");
+        }
+    } else {
+        header("Location:../user-edit.php?id=$id&m=d-1");
+    }
+}
+?>
---
+++ admin/user.php
@@ -0,0 +1,179 @@
+<?php
+session_start();
+include("../koneksi/koneksi.php");
+include("./classes/View.php");
+$pageTitle = "Users";
+$pageSeq = 8;
+?>
+<!doctype html>
+<html lang="en">
+<?php include("./chunks/head.php") ?>
+
+<body class="antialiased">
+  <div class="wrapper">
+    <?php include("./chunks/sidebar.php") ?>
+    <div class="page-wrapper">
+      <div class="container-fluid">
+        <!-- Page title -->
+        <div class="page-header d-print-none">
+          <div class="row align-items-center">
+            <div class="col">
+              <div class="page-pretitle">
+                Overview
+              </div>
+              <h2 class="page-title">
+                User
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="page-body">
+        <div class="container-xl">
+          <div class="row row-cards">
+            <div class="col-12">
+              <?php
+              if (isset($_GET['m'])) {
+                $alert = explode("-", $_GET['m']);
+                switch ($alert[1]) {
+                  case 1:
+                    echo View::createAlert($alert[0], "Berhasil mengedit user!");
+                    break;
+                  case 2:
+                    echo View::createAlert($alert[0], "Berhasil menghapus user!");
+                    break;
+                  case 3:
+                    echo View::createAlert($alert[0], "Gagal menghapus user!");
+                    break;
+                  default:
+                    break;
+                }
+              }
+              ?>
+              <div class="card">
+                <div class="card-header">
+                  <h3 class="card-title">Data User</h3>
+                </div>
+                <div class="card-body border-bottom py-3">
+                  <div class="d-flex">
+                    <div class="text-muted">
+                      Search:
+                      <div class="ms-md-2 d-inline-block">
+                        <input type="text" class="form-control form-control-sm" aria-label="Search Admin">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="table-responsive">
+                  <table class="table card-table table-vcenter datatable">
+                    <thead>
+                      <tr>
+                        <th>No.</th>
+                        <th>User</th>
+                        <th>Total Pembelian</th>
+                        <th>Verified</th>
+                        <th class="text-center text-md-end">Action</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <?php
+                      $query = "SELECT id, nama, email, foto, verified FROM tbl_user WHERE deleted IS NULL OR deleted = 0";
+                      $ret = mysqli_query($koneksi, $query);
+                      $jum = mysqli_num_rows($ret);
+                      if ($jum > 0) {
+                        $no = 0;
+                        while ($data = mysqli_fetch_row($ret)) {
+                          $no++;
+                          $id_user = $data[0];
+                          $nama_user = $data[1];
+                          $email_user = $data[2];
+                          $foto_user = $data[3];
+                          $user_verified = $data[4];
+                          $inisial = explode(" ", $nama_user);
+                          if (sizeof($inisial) > 1) {
+                            $inisial = $inisial[0][0] . $inisial[1][0];
+                          } else {
+                            $inisial = $inisial[0][0];
+                          }
+                      ?>
+                          <tr>
+                            <td data-label="No."><span class="text-muted"><?= $no ?></span></td>
+                            <td data-label="User">
+                              <div class="d-flex align-items-center py-1">
+                                <span class="avatar me-2" <?= $foto_user != null ? "style='background-image: url(./assets/images/users/$foto_user);'" : "" ?>><?= $foto_user == null ? $inisial : '' ?></span>
+                                <div class="flex-fill">
+                                  <div class="font-weight-medium">
+                                    <?= $nama_user ?>
+                                  </div>
+                                  <div class="text-muted">
+                                    <?= $email_user ?>
+                                  </div>
+                                </div>
+                              </div>
+                            </td>
+                            <td>
+                              Rp. 123.456
+                            </td>
+                            <td>
+                              <span class="badge bg-<?= $user_verified == 1 ? 'success' : 'danger' ?> me-1"></span>
+                              <?= $user_verified == 1 ? 'True' : 'False' ?>
+                            </td>
+                            <td data-label="Action">
+                              <div class="btn-list justify-content-md-end flex-nowrap">
+                                <a href="user-detail.php?id=<?= $id_user ?>" class="btn btn-outline-success">
+                                  Detail
+                                </a>
+                                <a href="user-edit.php?id=<?= $id_user ?>" class="btn btn-outline-primary">
+                                  Edit
+                                </a>
+                                <a href="javascript:if(confirm('Anda yakin ingin menghapus data <?= $nama_user ?>?'))window.location.href = './handlers/user.php?hapus=<?= $id_user ?>'" class="btn btn-outline-danger">
+                                  Hapus
+                                </a>
+                              </div>
+                            </td>
+                          </tr>
+                      <?php }
+                      } ?>
+                    </tbody>
+                  </table>
+                </div>
+                <div class="card-footer d-flex align-items-center">
+                  <p class="m-0 text-muted">Showing <span>1</span> to <span>8</span> of <span>16</span> entries</p>
+                  <ul class="pagination m-0 ms-auto">
+                    <li class="page-item disabled">
+                      <a class="page-link" href="#" tabindex="-1" aria-disabled="true">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                          <polyline points="15 6 9 12 15 18" />
+                        </svg>
+                      </a>
+                    </li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item active"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item"><a class="page-link" href="#">4</a></li>
+                    <li class="page-item"><a class="page-link" href="#">5</a></li>
+                    <li class="page-item">
+                      <a class="page-link" href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                          <polyline points="9 6 15 12 9 18" />
+                        </svg>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <?php include("./chunks/footer.php") ?>
+    </div>
+  </div>
+  <!-- Libs JS -->
+  <!-- Tabler Core -->
+  <script src="./dist/js/tabler.min.js"></script>
+</body>
+
+</html>
