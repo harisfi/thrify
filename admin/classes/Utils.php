d3ea7c590ad0aafcd8747ddd9a1b2a32d5c98fb4
harisfi <acc.cp.haris@gmail.com>
2021-06-05 22:22:17 +0700

    Add dashboard page

--- admin/classes/Utils.php
+++ admin/classes/Utils.php
@@ -4,5 +4,16 @@ class Utils {
         $n = strrpos($s,".");
         return ($n===false) ? "" : substr($s,$n+1);
     }
+
+    public static function formatNumber($n) {
+        if ($n < 1000000) {
+            $n_format = number_format($n);
+        } else if ($n < 1000000000) {
+            $n_format = number_format($n / 1000000, 1) . 'M';
+        } else {
+            $n_format = number_format($n / 1000000000, 1) . 'B';
+        }
+        return $n_format;
+    }
 }
 ?>
